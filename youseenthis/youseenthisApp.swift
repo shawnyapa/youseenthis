fa08fb0279ec17327544c74a34be700917d01d53
Ashanta Yapa <shawnyapa@gmail.com>
2022-02-20 18:47:18 -0800

    Initial Commit

---
+++ youseenthis/youseenthisApp.swift
@@ -0,0 +1,17 @@
+//
+//  youseenthisApp.swift
+//  youseenthis
+//
+//  Created by Shawn Yapa on 2/20/22.
+//
+
+import SwiftUI
+
+@main
+struct youseenthisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
